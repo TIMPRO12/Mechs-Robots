d4ad4652d751807d39651ce6cf5d43cc9d1507c2
Cycil <coolgoldenfreddy19@gmail.com>
2024-12-17 10:53:33 -0500

    day4

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 import pymysql
 from dynaconf import Dynaconf
 
@@ -8,6 +8,9 @@ app = Flask(__name__)
 conf = Dynaconf(
      settings_file = [ 'settings.toml' ]
 )
+
+
+app.secret_key = [ "settings.toml" ]
 
 def connect_db():
     conn = pymysql.connect(
@@ -47,3 +50,70 @@ def product_browse():
     return render_template('browse.html.jinja',  products = results)
 
     
+@app.route("/product/<product_id>")
+
+def product_page(product_id):
+
+    conn = connect_db()
+
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM `Product` WHERE `id` = {product_id}; ")
+
+    results = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    return render_template('product.html.jinja',  product = results)
+
+
+@app.route("/signup", methods=["POST", "GET"])
+
+def  signup():
+
+    if request.method == 'POST':
+
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        emailadd = request.form["email"]
+        password = request.form["password"]
+        conpasswrd = request.form["conpassword"]
+        conn = connect_db()
+
+        cursor = conn.cursor()
+        if conpasswrd != password:
+            flash("The passwords don't match.")
+        else:
+
+        
+
+            try:
+                cursor.execute(f"""
+
+            
+                INSERT INTO `Customer` 
+                    (`First_name`, `Last_name`, `Email`, `password`)
+                VALUES
+                    ('{first_name}', '{last_name}', '{emailadd}', '{password}');
+            """)
+            except pymysql.err.IntegrityError:
+                flash("Sorry, account using this information has already been made. Please try a differenet email.")   
+                
+            else:
+                return redirect("/signin")
+            finally:       
+                cursor.close()
+                conn.close()
+        
+            
+
+
+
+
+    return render_template('signup.html.jinja')
+
+@app.route("/signin")
+
+def  signin():
+    return render_template('signin.html.jinja')
